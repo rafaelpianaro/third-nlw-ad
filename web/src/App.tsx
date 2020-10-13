bcae9801b7492ced6b843fe87b60d64ad8239699
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-13 14:58:36 -0300

    finalizando styles da landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 
 import './styles/global.css'
 import './styles/pages/landing.css'
@@ -19,7 +20,7 @@ function App() {
           <span>Santa Catarina</span>
         </div>
           <a href="#" className="enter-app">
-            
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
           </a>
       </div>
     </div>
