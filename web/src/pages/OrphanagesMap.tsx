f83fdc07a28bd7592bb3e04be7fdfa7abe06783e
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-13 16:44:40 -0300

    importanto leaflet.css

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { FiPlus } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
@@ -20,7 +23,9 @@ function OrphanagesMap() {
                     <span>Paraná</span>
                 </footer>
             </aside>
-            <div></div>
+            <Map center={[-25.4089886,-49.2942932]} zoom={15} style={{ width: '100%', height: '100%' }}> 
+                <TileLayer url="https://a.tile.openstreetmap.org/${z}/${x}/${y}.png" />
+            </Map>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
             </Link>
