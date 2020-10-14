d8d5f2ab4f6d46f983a06e89b21e1334ed47f03d
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-14 13:56:26 -0300

    explicando server.ts - rotas, recursos, métodos, parâmetros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,22 @@ import express from 'express'
 
 const app = express()
 
+// Rota = conjunto
+// Recurso = usuário
+
+// Métodos HTTP = GET, POST, PUT, DELETE
+
+// GET = Buscar uma informação (Lista, Item)
+// POST = Criando uma informação
+// PUT = Editando uma informação
+// DELETE = Deletando uma informação
+
+// Parâmetros
+
+// Query Params: http://localhost:3333/users?search=rafael&page=2
+// Route Params: DELETE http://localhost:3333/users/1 (Identificar um recurso)
+// Body: http://localhost:3333/users/ (Identificar um recurso)
+
 app.get('/users',(request, response) => {
     return response.json({message: 'Hello World'})
 })
