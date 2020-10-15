6b9535ab49fc7de6bc052dd39f2483804abd9dd1
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-14 21:51:09 -0300

    criando migration images com npx typeorm migration:create -n create_images

---
+++ backend/src/database/migrations/1602723032699-create_images.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImages1602723032699 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
