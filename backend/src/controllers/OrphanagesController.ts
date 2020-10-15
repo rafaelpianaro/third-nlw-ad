7fde157b910fbe78348635437dee5f7e86ee6124
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-14 23:44:17 -0300

    salvando images com orfanato no create de OrphanagesControllers

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -30,6 +30,11 @@ export default {
         } = request.body
     
         const orphanagesRepository = getRepository(Orphanage)
+
+        const requestImages = request.files as Express.Multer.File[]
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -38,7 +43,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         })
         await orphanagesRepository.save(orphanage)
         return response.status(201).json({orphanage})
