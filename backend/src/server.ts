37136b4fbfffe91ce8db985cd761d4c62f8d1bc2
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-15 17:39:43 -0300

    adicionado cors no server.ts com app.use(cors())

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,12 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+// cors para produção
+// app.use(cors({
+//     origin: []
+// }))
+
+app.use(cors())
 app.use(express.json());
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
