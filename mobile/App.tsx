dab5aa5ddc7134d34bea121c5c9bf9e3bd77ee30
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-17 19:46:07 -0300

    importado Routes em App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { useFonts } from 'expo-font'
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 
+import Routes from './src/routes'
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
@@ -16,6 +18,6 @@ export default function App() {
   }
 
   return (
-    
+    <Routes />
   );
 }
