bff7fbe9814a49fa3d122d860291e60bd1f04239
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-16 22:03:43 -0300

    adicionado estilos para o footer mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
 
 import mapMarker from './src/images/map-marker.png'
 
@@ -27,6 +28,14 @@ export default function App() {
         </Callout>
       </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOraphanagesButton} onPress={() => {}}>
+          <Feather name="plus" size={20} color={"#fff"} />
+        </TouchableOpacity>
+
+      </View>
     </View>
   );
 }
@@ -53,6 +62,34 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#0089a5',
     fontSize: 14
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 3
+  },
+
+  footerText: {
+    color: '#8fa7b3'
+  },
+
+  createOraphanagesButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 
 
