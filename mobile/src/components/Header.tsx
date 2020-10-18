0a8068258c556e297d3294cab5b4b5eedeebe50b
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-17 21:32:08 -0300

    adicionado navegação para botões do Header

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import { BorderlessButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 interface HeaderProps {
     // title?: string //quando não for obrigatório
@@ -9,13 +10,19 @@ interface HeaderProps {
 }
 
 export default function Header({ title }: HeaderProps) {
+    const navigation = useNavigation()
+
+    function handleGoBackToAppHomepage() {
+        navigation.navigate('OrphanagesMap')
+    }
+
     return (
         <View style={styles.container}>
-            <BorderlessButton onPress={() => {}}>
+            <BorderlessButton onPress={navigation.goBack}>
                 <Feather name="arrow-left" size={24} color="#15b6d6" />
             </BorderlessButton>
             <Text style={styles.title}>{title}</Text>
-            <BorderlessButton onPress={() => {}}>
+            <BorderlessButton onPress={handleGoBackToAppHomepage}>
                 <Feather name="x" size={24} color="#ff669d" />
             </BorderlessButton>
         </View>
