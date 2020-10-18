f195b66f984c498d4a1969e20ad5dcdb4ad9c757
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-17 23:34:49 -0300

    modificado baseURL em api.ts

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: ''
+    // baseURL: 'http://localhost:3333'
+    // celular físico
+    baseURL: 'http://192.168.0.17:3333'
+    // para emulador rodar primeiro no terminal
+    //  adb reverse tcp:3333 tcp:3333
 })
 
 export default api
